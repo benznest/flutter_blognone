7561a0f7bf502e930327324142687b6ec466167b
Panuwat Jantawee <benz7net@gmail.com>
2019-07-24 23:32:41 +0700

    add html

--- example/lib/ui/screens/node_content_screen.dart
+++ example/lib/ui/screens/node_content_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blognone/dao/blognone_node_content_dao.dart';
 import 'package:flutter_blognone/flutter_blognone.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class NodeContentScreen extends StatefulWidget {
   final int nodeId;
@@ -14,7 +15,6 @@ class NodeContentScreen extends StatefulWidget {
 
 class _NodeContentScreenState extends State<NodeContentScreen> {
   FlutterBlognone bn = FlutterBlognone();
-  WebViewController _webViewController;
 
   @override
   Widget build(BuildContext context) {
@@ -41,36 +41,38 @@ class _NodeContentScreenState extends State<NodeContentScreen> {
   Widget buildNodeContentContainer(BlognoneNodeContentDao node) {
     return CustomScrollView(slivers: [
       SliverList(
-          delegate: SliverChildListDelegate([
-        Container(
-            padding: EdgeInsets.all(16),
-            width: double.infinity,
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey[300], width: 1),
-              borderRadius: BorderRadius.circular(6.0),
-            ),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-              Row(children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        node.title,
-                        style: TextStyle(fontSize: 16),
-                      )
-                    ],
-                  ),
-                )
-              ]),
-              SizedBox(height: 8),
-              Text("โดย " + node.writer + " " + node.date, style: TextStyle(fontSize: 12, color: Colors.grey[800])),
-              buildTagsContainer(node.tags),
-            ])),
+        delegate: SliverChildListDelegate([
+          Container(
+              padding: EdgeInsets.all(16),
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey[300], width: 1),
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                Row(children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          node.title,
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    ),
+                  )
+                ]),
+                SizedBox(height: 8),
+                Text("โดย " + node.writer + " " + node.date, style: TextStyle(fontSize: 12, color: Colors.grey[800])),
+                buildTagsContainer(node.tags),
+                Html(
+                    data: """ ${node.contentFull} """)
+              ])),
 
-      ]),
+        ]),
       ),
     ]);
   }
@@ -78,16 +80,20 @@ class _NodeContentScreenState extends State<NodeContentScreen> {
   buildTagsContainer(List<String> tags) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 6),
-      child: Wrap(spacing: 4, alignment: WrapAlignment.start, runAlignment: WrapAlignment.start, direction: Axis.horizontal, children: <Widget>[
-        for (String tag in tags)
-          Container(
-              padding: EdgeInsets.all(4),
-              decoration: BoxDecoration(border: Border.all(color: Colors.grey[300], width: 1)),
-              child: Text(
-                tag,
-                style: TextStyle(fontSize: 12),
-              ))
-      ]),
+      child: Wrap(spacing: 4,
+          alignment: WrapAlignment.start,
+          runAlignment: WrapAlignment.start,
+          direction: Axis.horizontal,
+          children: <Widget>[
+            for (String tag in tags)
+              Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(border: Border.all(color: Colors.grey[300], width: 1)),
+                  child: Text(
+                    tag,
+                    style: TextStyle(fontSize: 12),
+                  ))
+          ]),
     );
   }
 }
